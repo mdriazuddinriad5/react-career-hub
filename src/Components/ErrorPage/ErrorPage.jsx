f591628de2ae501f3c9caed0519323e88220ba15
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-09-21 23:05:15 +0600

    Nested route and navbar added

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Opssss</h2>
+            <Link to='/'>Go back home</Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
